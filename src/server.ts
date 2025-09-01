d0285bd8fbfa27379357e65e18eb6d5819eafebc
SaumoB <saumobanik@gmail.com>
2025-09-01 20:21:58 +0530

    fix build code

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import mongoose from "mongoose";
-import orders from "./routes/order.ts";
-import kitchens from "./routes/kitchen.ts";
-import myorders from "./routes/myorder.ts";
+import orders from "./routes/order.js";
+import kitchens from "./routes/kitchen.js";
+import myorders from "./routes/myorder.js";
 
 const app = express();
 
@@ -19,8 +19,8 @@ app.get('/',(req,res)=> {
 //   } catch (e) { next(e);}
 // })
 app.use(helmet());
-app.use(cors()); // allow everything
-//app.use(cors({ origin: process.env.FRONTEND_ORIGIN?.split(",") || true }));
+// app.use(cors()); // allow everything
+app.use(cors({ origin: process.env.FRONTEND_ORIGIN?.split(",") || true }));
 app.use(express.json()); // for normal routes (webhook route handles raw body itself)
 
 app.use("/api/orders", orders);
