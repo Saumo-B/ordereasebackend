7f43a953f28553b5b26e16529796264fcbc20049
SaumoB <saumobanik@gmail.com>
2025-09-26 18:16:48 +0530

    turn off auth

--- dist/server.js
+++ dist/server.js
@@ -73,8 +73,17 @@ app.use((0, cors_1.default)({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
-app.options("*");
+app.options("*", (0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 // app.use(authenticate);   // populate req.user
 // app.use(autoPermission); // enforce from central map
 // API routes
--- src/server.ts
+++ src/server.ts
@@ -80,8 +80,17 @@ app.use(
     credentials: true,
   })
 );
-app.options("*")
+app.options("*", cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // app.use(authenticate);   // populate req.user
 // app.use(autoPermission); // enforce from central map
