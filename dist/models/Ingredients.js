3b39be9dca386c86208b67de9e448914ef6d2201
SaumoB <saumobanik@gmail.com>
2025-09-26 20:48:30 +0530

    update ingredients api

--- dist/models/Ingredients.js
+++ dist/models/Ingredients.js
@@ -9,7 +9,7 @@ const IngredientSchema = new mongoose_1.default.Schema({
     name: { type: String, required: true, unique: true },
     unit: { type: String, required: true },
     quantity: { type: Number, required: true, default: 0 }, // actual stock
-    branch: { type: mongoose_1.default.Schema.Types.ObjectId, ref: "Branch", required: true, immutable: true }, // 🔹 branch required
+    branch: { type: mongoose_1.default.Schema.Types.ObjectId, ref: "Branch", required: true }, // 🔹 branch required
     reservedQuantity: { type: Number, default: 0 }, // reserved for unpaid orders
     lowStockThreshold: { type: Number, default: 0 }, // user-defined warning threshold
 }, { timestamps: true });
